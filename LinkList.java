0e20be589578bfa255cc649f42968f2d36fa413d
pranav21999 <pranavdani99@gmail.com>
2021-06-30 20:00:50 +0530

    added methods for creation,insertion and display for the  LinkList

---
+++ LinkList.java
@@ -0,0 +1,49 @@
+package ds;
+import java.util.LinkedList;
+import ds.LinkList.Node;
+
+public class LinkList {
+	//creating head node 
+	Node head;
+	class Node{
+		Node next;
+		int data;
+		//constructor 
+		public Node(int data) {
+			super();
+			this.next = next;
+			this.data = data;
+		}
+	}
+	//method to insert data in LinkList
+	public Node insert(int data) 
+	{	
+		 Node newNode = new Node(data);
+		 if (head == null) 
+		 {
+			 head = newNode;
+		 } 
+		 else
+		 {
+			 Node temp = head;				 
+			 while (temp.next != null) 
+			 {
+			 temp = temp.next;
+			 }
+			 temp.next = newNode;
+		 }
+		 return newNode;
+	 }
+	//method to Display LinkList
+	public void print() 
+	 {			 
+		 Node temp = head;
+		 while (temp != null)
+		 {
+			 System.out.print(temp.data + " -> ");
+			 temp = temp.next;
+		 }
+		 System.out.println();
+	 }
+	
+}
---
+++ MainList.java
@@ -0,0 +1,14 @@
+package ds;
+
+public class MainList {
+	//main method
+	public static void main(String[] args) 
+	{	
+		LinkList linkList=new LinkList();
+		linkList.insert(70);
+		linkList.insert(30);
+		linkList.insert(56);
+		linkList.print();
+	}
+
+}
